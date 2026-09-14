c0cc090542638690bd8c0907a48677f6486f0380
unknown <rafael24_es@hotmail.com>
2026-09-14 15:38:19 -0500

    migration fix

--- Backend/prisma/migrations/20260914132823_set_sede_media_and_add_intervalo_act_ranking/migration.sql
+++ Backend/prisma/migrations/20260914132823_set_sede_media_and_add_intervalo_act_ranking/migration.sql
@@ -1,36 +1,38 @@
-/*
-  Warnings:
-
-  - The primary key for the `asignacionmedia` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `campaign_id` on the `asignacionmedia` table. All the data in the column will be lost.
-  - Added the required column `sede_id` to the `AsignacionMedia` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- DropForeignKey
-ALTER TABLE `asignacionmedia` DROP FOREIGN KEY `AsignacionMedia_campaign_id_fkey`;
+ALTER TABLE `AsignacionMedia`
+DROP FOREIGN KEY `AsignacionMedia_campaign_id_fkey`;
 
 -- DropIndex
-DROP INDEX `AsignacionMedia_campaign_id_idx` ON `asignacionmedia`;
+DROP INDEX `AsignacionMedia_campaign_id_idx`
+ON `AsignacionMedia`;
 
--- DropIndex
-DROP INDEX `AsignacionMedia_campaign_id_prioridad_idx` ON `asignacionmedia`;
+DROP INDEX `AsignacionMedia_campaign_id_prioridad_idx`
+ON `AsignacionMedia`;
 
 -- AlterTable
-ALTER TABLE `asignacionmedia` DROP PRIMARY KEY,
+ALTER TABLE `AsignacionMedia`
+    DROP PRIMARY KEY,
     DROP COLUMN `campaign_id`,
     ADD COLUMN `sede_id` INTEGER NOT NULL,
     ADD PRIMARY KEY (`sede_id`, `media_id`);
 
 -- AlterTable
-ALTER TABLE `configranking` ADD COLUMN `intervalo_actualizacion` INTEGER NOT NULL DEFAULT 15,
+ALTER TABLE `ConfigRanking`
+    ADD COLUMN `intervalo_actualizacion` INTEGER NOT NULL DEFAULT 15,
     ADD COLUMN `proxima_actualizacion` DATETIME(3) NULL,
     ADD COLUMN `ultima_actualizacion` DATETIME(3) NULL;
 
 -- CreateIndex
-CREATE INDEX `AsignacionMedia_sede_id_idx` ON `AsignacionMedia`(`sede_id`);
+CREATE INDEX `AsignacionMedia_sede_id_idx`
+ON `AsignacionMedia`(`sede_id`);
 
--- CreateIndex
-CREATE INDEX `AsignacionMedia_sede_id_prioridad_idx` ON `AsignacionMedia`(`sede_id`, `prioridad`);
+CREATE INDEX `AsignacionMedia_sede_id_prioridad_idx`
+ON `AsignacionMedia`(`sede_id`, `prioridad`);
 
 -- AddForeignKey
-ALTER TABLE `AsignacionMedia` ADD CONSTRAINT `AsignacionMedia_sede_id_fkey` FOREIGN KEY (`sede_id`) REFERENCES `Sede`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AsignacionMedia`
+ADD CONSTRAINT `AsignacionMedia_sede_id_fkey`
+FOREIGN KEY (`sede_id`)
+REFERENCES `Sede`(`id`)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
