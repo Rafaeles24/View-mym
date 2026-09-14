704250e39c9bc948cc1afb120406812e14e0dd0f
unknown <rafael24_es@hotmail.com>
2026-09-14 15:41:43 -0500

    fix migration v2

--- Backend/prisma/migrations/20260914193633_update_config_ranking_intervalo/migration.sql
+++ Backend/prisma/migrations/20260914193633_update_config_ranking_intervalo/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE `configranking` ADD COLUMN `hora_fin_actualizacion` VARCHAR(5) NOT NULL DEFAULT '16:00',
+ALTER TABLE `Configranking` ADD COLUMN `hora_fin_actualizacion` VARCHAR(5) NOT NULL DEFAULT '16:00',
     ADD COLUMN `hora_inicio_actualizacion` VARCHAR(5) NOT NULL DEFAULT '07:00';
