bbb2b9084b79c6ca69bc3ef3c679fd033dd72b04
unknown <rafael24_es@hotmail.com>
2026-09-21 11:35:02 -0500

    Delete Captions

--- Backend/src/caption/caption.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CaptionService } from './caption.service';
-import { CaptionController } from './caption.controller';
-
-@Module({
-  controllers: [CaptionController],
-  providers: [CaptionService],
-})
-export class CaptionModule {}
